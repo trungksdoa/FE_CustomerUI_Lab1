c821478787c9fa87a844a027d6882d309f5f4ea4
trungksdoa <55863018+trungksdoa@users.noreply.github.com>
2022-06-24 02:22:17 +0700

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://serveramazone2022.herokuapp.com//api/v1'
+  apiBaseUrl: 'https://serveramazone2022.herokuapp.com/api/v1'
 };
