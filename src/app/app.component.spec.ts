a86b89969751a38b5373498e84490ab29c1f28e7
trungksdoa <55863018+trungksdoa@users.noreply.github.com>
2022-06-24 01:52:21 +0700

    updates to deploy to heroku

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
 
   it(`should have as title 'customer-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app :any= fixture.componentInstance;
     expect(app.title).toEqual('customer-ui');
   });
 
