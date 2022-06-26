3e48368cf492b22732f6a6eec00fb6e62d3caf58
trungksdoa <55863018+trungksdoa@users.noreply.github.com>
2022-06-26 15:05:20 +0700

    update

--- src/app/api/product/product.ts
+++ src/app/api/product/product.ts
@@ -1,18 +1,13 @@
 import { Category } from "../category/category";
+import { FileDB } from "./FileDB";
 
 export interface Product extends Category {
-
-    id: number;
-    name: string;
-    description: string;
-    imageurl: string;
-    price: number;
-    createdAt: Date;
-    updatedAt: Date;
-    catagory: Category;
-    
-  }
-
- 
-
-
+  id: number;
+  name: string;
+  description: string;
+  imageurl: FileDB;
+  price: number;
+  createdAt: Date;
+  updatedAt: Date;
+  catagory: Category;
+}
