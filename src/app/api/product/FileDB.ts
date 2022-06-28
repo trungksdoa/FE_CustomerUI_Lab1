6ffc5df8357ede6568269fd82dc9ea46ecf1d8e3
nguyencaothai <lanhoxga@gmail.com>
2022-06-28 13:42:36 +0700

    update

--- src/app/api/product/FileDB.ts
+++ src/app/api/product/FileDB.ts
@@ -1,14 +1,8 @@
 export interface FileDB {
-<<<<<<< HEAD
+
     id:string,
     name:string,
     type:string,
     data:any
   }
-=======
-  id:string,
-  name:string,
-  type:string,
-  data:any
-}
->>>>>>> 14c7a86441fbcd81a97ba3c5d57b1501c5d8806a
+
